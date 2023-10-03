e73ab435dc7fa88aea242cda53ac703651a2d7a0
universeYong <boyd0509@gmail.com>
2023-10-03 15:55:53 +0900

    add result page

---
+++ src/asset/data/resultdata.js
@@ -0,0 +1,11 @@
+import puppy from '../dog/puppy.jpg';
+
+export const ResultData = [
+    {
+        id: 1,
+        name : '러시안 블루',
+        best : 'ESTJ',
+        desc : '겁이 많아요',
+        image : `${puppy}`
+    },
+]
